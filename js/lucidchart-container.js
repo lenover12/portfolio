c96c36f66b6f311421fb3e1e182e9de0f07247ba
lenover12 <lennymcdonald247@hotmail.com>
2024-03-11 18:35:01 +1100

    Update Resume & Add lucidchart fallback

--- js/lucidchart-container.js
+++ js/lucidchart-container.js
@@ -1,14 +1,23 @@
 window.addEventListener('DOMContentLoaded', function() {
   const lucidchartContainer = document.querySelector('.lucidchart-container');
   const lucidchartFrame = document.getElementById('lucidchart-frame');
+  const lucidchartFallbackImage = document.querySelector('.lucidchart-fallback-image');
 
   function setSquareHeight() {
       const width = lucidchartContainer.offsetWidth;
       lucidchartContainer.style.height = width + 'px';
       lucidchartFrame.style.height = '100%';
+      lucidchartFallbackImage.style.width = '100%';
+      lucidchartFallbackImage.style.height = '100%';
   }
 
   setSquareHeight();
+
+  // Hide the iframe and show the fallback image if the iframe fails to load
+  lucidchartFrame.addEventListener('error', function() {
+      lucidchartFrame.style.display = 'none';
+      lucidchartFallbackImage.style.display = 'block';
+  });
 
   window.addEventListener('resize', function() {
       setSquareHeight();
